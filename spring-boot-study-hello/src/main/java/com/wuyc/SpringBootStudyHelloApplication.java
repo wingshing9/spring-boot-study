b64e68f3814e5b682ef6a90763507cf6d3584ab2
wingshing9 <wingshing9@163.com>
2020-04-28 14:12:46 +0800

    新增项目

---
+++ spring-boot-study-hello/src/main/java/com/wuyc/SpringBootStudyHelloApplication.java
@@ -0,0 +1,13 @@
+package com.wuyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyHelloApplication.class, args);
+    }
+
+}
