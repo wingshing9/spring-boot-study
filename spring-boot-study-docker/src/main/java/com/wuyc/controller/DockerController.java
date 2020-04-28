1fccfbbc26b86f4a62c883a2f142ca90ee0820c8
wingshing9 <wingshing9@163.com>
2020-04-28 17:53:04 +0800

    新增项目

---
+++ spring-boot-study-docker/src/main/java/com/wuyc/controller/DockerController.java
@@ -0,0 +1,12 @@
+package com.wuyc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+    @RequestMapping("/")
+    public String index(){
+        return "Hello Docker";
+    }
+}
---
+++ spring-boot-study-docker/src/test/java/com/wuyc/SpringBootStudyDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
