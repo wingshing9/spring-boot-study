aad8fdc45be4ba59560f5b30c9e16e77153f0d1e
wingshing9 <wingshing9@163.com>
2022-05-26 15:03:08 +0800

    第三次提交

--- spring-boot-study-hello/src/main/java/com/wuyc/HelloWorld.java
+++ spring-boot-study-hello/src/main/java/com/wuyc/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("111111111111");
 		System.out.println("222222222222");
+		System.out.println("333333333333");
     }
 
 }
